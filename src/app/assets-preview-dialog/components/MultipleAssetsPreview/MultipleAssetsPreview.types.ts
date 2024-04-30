54fd1b84360fe370faa5c3945a3d2418f7af66a3
Pierniki <mszczep4niak@gmail.com>
2024-04-30 16:01:34 +0200

    refactor: cleanup unused components

--- src/app/assets-preview-dialog/components/MultipleAssetsPreview/MultipleAssetsPreview.types.ts
+++
@@ -1,8 +0,0 @@
-import { Asset } from '@/app/asset-field/components/AssetCard/AssetCard.types';
-import { Nullable } from '@/types/common';
-
-export interface MultipleAssetsPreview {
-  assets: Asset[];
-  onCloseDialog: (assets: Nullable<Asset[]>) => void;
-  handleClick: (asset: Asset) => void;
-}
--- src/app/assets-preview-dialog/components/SingleAssetPreview/SingleAssetPreview.types.ts
+++
@@ -1,9 +0,0 @@
-import { Asset } from '@/app/asset-field/components/AssetCard/AssetCard.types';
-import { Nullable } from '@/types/common';
-
-export interface SingleAssetPreview {
-  singleAssetPreview: Asset;
-  onCloseDialog: (assets: Nullable<Asset[]>) => void;
-  isFromSingleAssetField: boolean;
-  setSingleAssetPreview: (asset: Nullable<Asset>) => void;
-}
